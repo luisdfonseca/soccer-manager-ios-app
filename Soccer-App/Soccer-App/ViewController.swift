745fffea097f7891ebf72fd130a613cfa64fecbe
luis daniel fonseca <luisda_309@hotmail.com>
2016-11-26 14:03:28 -0500

    New Project file

---
+++ Soccer-App/Soccer-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soccer-App
+//
+//  Created by luis daniel fonseca on 11/26/16.
+//  Copyright © 2016 luis daniel fonseca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
