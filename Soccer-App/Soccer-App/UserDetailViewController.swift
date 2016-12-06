fdb281303b19324cc91fa6dd539e8c401a92b578
michael jason juarez <mjuar005@felucia.aul.fiu.edu>
2016-12-05 21:11:36 -0500

    This branch has my final touches

--- Soccer-App/Soccer-App/UserDetailViewController.swift
+++ Soccer-App/Soccer-App/UserDetailViewController.swift
@@ -1,18 +1,15 @@
-//
-//  UserDetailViewController.swift
-//  Soccer-App
-//
-//  Created by michael jason juarez on 12/2/16.
-//  Copyright © 2016 luis daniel fonseca. All rights reserved.
+//  PROGRAMMER:     Michael Juarez, Luis Daniel Fonseca
+//  PANTHERID:      1316430
+//  CLASS:          COP 465501 TR 5:00
+//  INSTRUCTOR:     Steve Luis  ECS 282
+//  ASSIGNMENT:     Final Project
+//  DUE:            Thursday 12/08/16
 //
 
 import CoreData
 import UIKit
 
 class UserDetailViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    enum error: ErrorType {
-        case bad
-    }
     
     @IBOutlet weak var fName: UITextField!
     @IBOutlet weak var lName: UITextField!
@@ -26,12 +23,14 @@ class UserDetailViewController: UIViewController, UITextFieldDelegate, UIImagePi
     @IBOutlet weak var birthdate: UITextField!
     
     @IBOutlet weak var navBar: UINavigationBar!
-
+    @IBOutlet weak var navbarItem: UINavigationItem!
+    
     @IBOutlet weak var playerImg: UIImageView!
     
     var image = UIImage()
     var index : Int = 0
     let playerstore = PlayerStore.sharedInstance
+    var thePlayer : Player = Player(playerNumber: "", firstName: "", lastName: "", phoneNumber: "", age: "", email: "", address: "", height: "", weight: "", birthdate: "")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,19 +45,27 @@ class UserDetailViewController: UIViewController, UITextFieldDelegate, UIImagePi
         self.height.delegate = self;
         self.weight.delegate = self;
         self.birthdate.delegate = self;
-        // Do any additional setup after loading the view.
         
-        playerImg.image = playerstore.getPlayerImage(index)
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
+        //Load text fields with information
         loadPlayerProperties()
+        
+        //Load the player's picture into the image
+        image = playerstore.getPlayerImage(index)
+        
+        //Get the player for the row selected
+        thePlayer = playerstore.getPlayer(self.index)
+        
     }
 
-    func loadPlayerProperties(){
+    override func viewWillAppear(animated: Bool) {
+        super.viewDidLoad()
         
-        let thePlayer : Player = playerstore.getPlayer(self.index)
+        //Update the image with whatever is stored in global 'image' variable
+        playerImg.image = image
+    }
+    
+    //Loads the text fields with the player selected
+    func loadPlayerProperties(){
         
         self.fName.text = thePlayer.firstName
         self.lName.text = thePlayer.lastName
@@ -71,16 +78,36 @@ class UserDetailViewController: UIViewController, UITextFieldDelegate, UIImagePi
         self.weight.text = thePlayer.weight
         self.birthdate.text = thePlayer.birthdate
         
-        self.title = thePlayer.firstName + " " + thePlayer.lastName        
+        navbarItem.title = thePlayer.firstName + " " + thePlayer.lastName
     }
     
     
     @IBAction func saveButton(sender: AnyObject) {
         
-        var thePlayer : Player = Player(playerNumber: "", firstName: "", lastName: "", phoneNumber: "", age: "", email: "", address: "", height: "", weight: "", birthdate: "")
+        // If player number is the same, then update
+        if self.playerNumber.text == thePlayer.playerNumber {
+            thePlayer.firstName = self.fName.text!
+            thePlayer.lastName = self.lName.text!
+            thePlayer.playerNumber = self.playerNumber.text!
+            thePlayer.phoneNumber = self.phoneNumber.text!
+            thePlayer.age = self.age.text!
+            thePlayer.email = self.email.text!
+            thePlayer.address = self.address.text!
+            thePlayer.height = self.height.text!
+            thePlayer.weight = self.weight.text!
+            thePlayer.birthdate = self.birthdate.text!
+            
+            playerstore.savePicture(image, player: thePlayer)
+            playerstore.updatePlayer(self.index, player: thePlayer)
+            
+            performSegueWithIdentifier("back2PlayersSegue", sender: self)
+        }
         
-        if !playerstore.checkPNumExists(self.playerNumber.text!) {
-       
+        // At this point, player number has changed, so check if it already exists, if it doesn't then proceed forward
+        else if !playerstore.checkPNumExists(self.playerNumber.text!) {
+            
+            
+            
             thePlayer.firstName = self.fName.text!
             thePlayer.lastName = self.lName.text!
             thePlayer.playerNumber = self.playerNumber.text!
@@ -92,10 +119,13 @@ class UserDetailViewController: UIViewController, UITextFieldDelegate, UIImagePi
             thePlayer.weight = self.weight.text!
             thePlayer.birthdate = self.birthdate.text!
         
+            playerstore.savePicture(image, player: playerstore.getPlayer(index))
             playerstore.updatePlayer(self.index, player: thePlayer)
-        
+            
             performSegueWithIdentifier("back2PlayersSegue", sender: self)
         }
+        
+        //The player number exists, blast user with message saying you can't use the same Jersey Number as another play
         else{
             let instructionAlert: UIAlertController = UIAlertController(title: "Number Already Exists", message: "Sorry, this number is already assigned to another player.", preferredStyle: .Alert)
             instructionAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
@@ -133,21 +163,16 @@ class UserDetailViewController: UIViewController, UITextFieldDelegate, UIImagePi
         imagePicker.delegate = self
         
         presentViewController(imagePicker, animated: true, completion: nil)
-    } // end pressedCamera
-    
-    
+    }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
 
         image = info[UIImagePickerControllerOriginalImage] as! UIImage
         
-        playerstore.savePicture(image, index: index)
-        
         playerImg.image = image
         
         dismissViewControllerAnimated(true, completion: nil)
-        
-    } // end imagePickerController (_:didFinish)
+    }
     
     
 
